9c67307dc5eafb2f64829d75a08b965040dd1ef0
Kayla Volk <kaylavolk8@gmail.com>
2022-11-30 20:53:39 -0500

    commit tree 7d214aa6d59eb0f94d1472931abb526f524e3ad7

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+const copyOfCats = [...cats, name]
+return copyOfCats
+}
+function prependCat(name){
+    const copyOfCats = [name, ...cats]
+return copyOfCats
+}
+function removeLastCat(){
+    const newCats = cats.slice(0, cats.length - 1)
+    return newCats
+} 
+function removeFirstCat(){
+    const newCats = cats.slice(1)
+    return newCats
+}
